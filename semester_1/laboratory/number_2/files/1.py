d5b9566b5bfed64fd7927405d70f5d7d6c73a263
Anlay <AnlayKor@yandex.ru>
2024-12-22 21:23:54 +0300

    lab2

---
+++ semester_1/laboratory/number_2/files/1.py
@@ -0,0 +1,14 @@
+def greet(name):
+    print(f'Привет, {name}!')
+
+
+def square(number):
+    return number ** 2
+
+
+def max_of_two(x, y):
+    return max(x, y)
+
+greet('Вика')
+print(square(3))
+print(max_of_two(2, 6))
---
+++ semester_1/laboratory/number_2/files/2.py
@@ -0,0 +1,5 @@
+def describe_person(name, age=30):
+    print(f'Имя: {name}, возраст: {age}')
+
+
+describe_person('Вика', age=18)
