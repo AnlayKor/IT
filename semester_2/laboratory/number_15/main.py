9714741b6eb643e8755b1fd9d06709fe629572eb
AnlayKor <150436643+AnlayKor@users.noreply.github.com>
2025-04-20 23:58:36 +0700

    Add files via upload

--- semester_2/laboratory/number_15/main.py
+++ semester_2/laboratory/number_15/main.py
@@ -1,1 +1,48 @@
+from fastapi import FastAPI
+import pyjokes
+import wikipedia
+from pydantic import BaseModel
+
+
+wikipedia.set_lang("ru")
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def joke():
+    return pyjokes.get_joke()
+
+
+@app.get("/{search}")
+def wikipedia_search(search: str):
+    return wikipedia.summary(search)
+
+
+@app.get("/multi/{request}")
+def multi_wikipedia_search(request: str, wiki_number: int):
+    titles = wikipedia.search(request, results=wiki_number)
+    result = ''
+    for i in titles:
+        result += wikipedia.summary(i) + (' ')
+    return result
+
+
+class Wiki(BaseModel):
+    search: str
+    wiki: str
+
+
+class WikiInput(BaseModel):
+    search: str
+
+
+@app.post("/", response_model=Wiki)
+def create_joke(wiki_input: WikiInput):
+    """Создание краткого содержания статьи"""
+    return Wiki(search=wiki_input.search, wiki=wikipedia.summary(wiki_input.search))
+
+
+#uvicorn main:app --reload
 
